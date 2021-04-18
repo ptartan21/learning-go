2c38f2c149934af8c2e9c27aee1d3d8c19d24944
Patrick Liu <patrick.liu01@gmail.com>
2021-04-18 03:16:01 -0500

    basic error handling

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Patrick")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(message)
 }
